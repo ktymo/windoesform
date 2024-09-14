fb20aa6c7fce348763c30cef423b85d1aeeb5d56
ktymo <142958234+ktymo@users.noreply.github.com>
2024-09-14 16:11:03 -0300

    primera subida

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TuPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.linkLogin = new System.Windows.Forms.LinkLabel();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.textPass = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(335, 179);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(142, 54);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Acceder";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(382, 42);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(62, 21);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "LOGIN";
+            // 
+            // linkLogin
+            // 
+            this.linkLogin.AutoSize = true;
+            this.linkLogin.Location = new System.Drawing.Point(346, 255);
+            this.linkLogin.Name = "linkLogin";
+            this.linkLogin.Size = new System.Drawing.Size(131, 13);
+            this.linkLogin.TabIndex = 0;
+            this.linkLogin.TabStop = true;
+            this.linkLogin.Text = "¿Olvidaste tu contraseña?";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUser.Location = new System.Drawing.Point(215, 100);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(400, 27);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "USUARIO";
+            this.txtUser.Click += new System.EventHandler(this.textUser_Enter);
+            this.txtUser.Enter += new System.EventHandler(this.textUser_Enter);
+            this.txtUser.Leave += new System.EventHandler(this.txtUser_Leave);
+            // 
+            // textPass
+            // 
+            this.textPass.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPass.ForeColor = System.Drawing.Color.DimGray;
+            this.textPass.Location = new System.Drawing.Point(215, 133);
+            this.textPass.Name = "textPass";
+            this.textPass.Size = new System.Drawing.Size(400, 27);
+            this.textPass.TabIndex = 2;
+            this.textPass.Text = "CONTRASEÑA";
+            this.textPass.Enter += new System.EventHandler(this.textPass_Enter);
+            this.textPass.Leave += new System.EventHandler(this.textPass_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 310);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.linkLogin);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TUPASS";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.LinkLabel linkLogin;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox textPass;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuPassword
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textUser_Enter(object sender, EventArgs e)
+        {
+            if(txtUser.Text == "Usuario")
+
+            {
+                txtUser.Text = "";
+                txtUser.ForeColor= Color.DarkGray;
+                
+            }
+        }
+
+        private void txtUser_Leave(object sender, EventArgs e)
+        {
+            if (txtUser.Text == " ")
+            {
+                txtUser.Text ="Usuario";
+                txtUser.ForeColor = Color.DarkGray;
+                
+            }
+        }
+
+        private void textPass_Enter(object sender, EventArgs e)
+        {
+
+            if (textPass.Text == "Contraseña")
+
+            {
+                textPass.Text = " ";
+                textPass.ForeColor = Color.DarkGray;
+                textPass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void textPass_Leave(object sender, EventArgs e)
+        {
+            if (textPass.Text == " ")
+            {
+                textPass.Text = "Contraseña";
+                textPass.ForeColor = Color.DarkGray;
+                textPass.UseSystemPasswordChar = false;
+            }
+        }
+
+       
+    }
+}
+.
